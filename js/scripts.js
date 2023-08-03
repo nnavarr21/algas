d67b22f3a112d3ff2ff409e3dc4384ac551ed063
Nicolas Navarrete <nnavarr21@gmail.com>
2023-08-03 17:49:28 -0400

    slider

--- js/scripts.js
+++ js/scripts.js
@@ -51,4 +51,20 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    
+    const slidesContainer = document.getElementById("slides-container");
+    const slide = document.querySelector(".slide");
+    const prevButton = document.getElementById("slide-arrow-prev");
+    const nextButton = document.getElementById("slide-arrow-next");
+    
+    nextButton.addEventListener("click", () => {
+      const slideWidth = slide.clientWidth;
+      slidesContainer.scrollLeft += slideWidth;
+    });
+    
+    prevButton.addEventListener("click", () => {
+      const slideWidth = slide.clientWidth;
+      slidesContainer.scrollLeft -= slideWidth;
+    });
 });
+
